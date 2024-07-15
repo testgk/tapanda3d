2dbf3eee91c5409cfac94e03da95a535f459e626
Gadi Klein <gadik@liveu.tv>
2024-07-15 18:27:21 +0300

    enums

---
+++ bullet.py
@@ -0,0 +1,4 @@
+from direct.showbase.ShowBase import ShowBase
+from panda3d.core import Vec3, Point3, BitMask32
+from panda3d.bullet import BulletWorld, BulletRigidBodyNode, BulletBoxShape
+
---
+++ enums/colors.py
@@ -0,0 +1,13 @@
+from enum import Enum
+from panda3d.core import Vec4, LVecBase4f
+
+
+class Color(Enum):
+    RED = LVecBase4f( 1, 0, 0, 1 )
+    GREEN = LVecBase4f( 0, 1, 0, 1 )
+    BLUE = LVecBase4f( 0, 0, 1, 1 )
+    WHITE = LVecBase4f( 1, 1, 1, 1 )
+    BLACK = LVecBase4f( 0, 0, 0, 1 )
+    YELLOW = LVecBase4f( 1, 1, 0, 1 )
+    CYAN = LVecBase4f( 0, 1, 1, 1 )
+    MAGENTA = LVecBase4f( 1, 0, 1, 1 )
