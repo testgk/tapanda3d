3aa8b24178d8f455a760e9a4c12a5f0c58405e38
Gadi Klein <gadik@liveu.tv>
2024-09-24 23:25:51 +0300

    split layers

--- terraincolision.py
+++ terraincolision.py
@@ -2,28 +2,22 @@ from panda3d.core import GeoMipTerrain, GeomNode, NodePath
 
 from customcollisionpolygon import CustomCollisionPolygon
 
-polygons = {}
+polygons = { }
+
 
 class TerrainCollision:
-    def __init__( self, terrain: GeoMipTerrain, physicsWorld, render ):
-        self.__terrain = terrain
-        self.__physicsWorld = physicsWorld
-        self.__render = render
+	def __init__( self, terrain: GeoMipTerrain ):
+		self.__terrain = terrain
 
-    def createTerrainCollision( self ):
-            root = self.__terrain.getRoot()
-            for child in root.getChildren():
-                terrainHeight = self.get_terrain_height( child.getX(), child.getY() )
-                print( f'{ terrainHeight} ' )
-                if isinstance( child.node(), GeomNode ):
-                    customCollisionPolygon = CustomCollisionPolygon( child, terrainHeight )
-                    customCollisionPolygon.attachCollisionNodeToTerrain()
-                    customCollisionPolygon.attachRigidBodyNodeToTerrain( self.__render )
-                    self.__physicsWorld.attachRigidBody( customCollisionPolygon.rigidBodyNode )
-            CustomCollisionPolygon.acquireAllNeighbors()
+	def createTerrainCollision( self ):
+		root = self.__terrain.getRoot()
+		for child in root.getChildren():
+			terrainHeight = self.get_terrain_height( child.getX(), child.getY() )
+			if isinstance( child.node(), GeomNode ):
+				customCollisionPolygon = CustomCollisionPolygon( child, terrainHeight )
+				customCollisionPolygon.attachCollisionNodeToTerrain()
+		CustomCollisionPolygon.acquireAllNeighbors()
 
-
-    def get_terrain_height(self, x, y):
-        """Return the height of the terrain at a given (x, y) position."""
-        return self.__terrain.getElevation(x, y) * 100
-
+	def get_terrain_height( self, x, y ):
+		"""Return the height of the terrain at a given (x, y) position."""
+		return self.__terrain.getElevation( x, y ) * 100
---
+++ terrainrigidbody.py
@@ -0,0 +1,18 @@
+from panda3d.core import GeoMipTerrain, GeomNode, NodePath
+
+from customcollisionpolygon import CustomCollisionPolygon
+from customrigidpolygon import CustomRigidPolygon
+
+
+class TerrainRigidBody:
+	def __init__( self, terrain: GeoMipTerrain, physicsWorld ):
+		self.__terrain = terrain
+		self.__physicsWorld = physicsWorld
+
+	def createTerrainRigidBody( self ):
+		root = self.__terrain.getRoot()
+		for child in root.getChildren():
+			if isinstance( child.node(), GeomNode ):
+				customRigidPolygon = CustomRigidPolygon( child )
+				customRigidPolygon.attachRigidBodyNodeToTerrain()
+				self.__physicsWorld.attachRigidBody( customRigidPolygon.rigidBodyNode )
