09a451f19b1ce0e722e1e0e408baa8a64f9e800a
Gadi Klein <gadik@liveu.tv>
2024-10-06 11:28:23 +0300

    entity buttons

---
+++ entities/entityphysics.py
@@ -0,0 +1,19 @@
+from panda3d.core import Vec3
+
+from entities.entity import Entity
+
+
+class Entityphysics:
+
+
+	@staticmethod
+	def applyVelocity( entity: Entity,  velocity: Vec3 = Vec3( 50, 0, 0) ):
+		if entity is None:
+			return
+		entity.rigidBodyNode.set_linear_velocity( velocity )
+
+	@staticmethod
+	def applyForce( entity: Entity,  force: Vec3 = Vec3( 50, 0, 0) ):
+		if entity is None:
+			return
+		entity.rigidBodyNode.applyForce( force )
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from direct.task import Task
 from panda3d.core import CollisionHandlerQueue, CollisionTraverser,Vec3
 
+from buttons.entitybuttons import EntityButtons
 from entities.entity import Entity
-from entities.full.movers.tank import Tank
-from entities.parts.engine import BasicEngine
+from entityloader import EntityLoader
 from enums.colors import Color
 
 from lights import Lights
@@ -14,11 +14,8 @@ from panda3d.bullet import BulletDebugNode, BulletWorld
 from buttons.camerabuttons import CameraButtons
 from selector.selector import Selector
 from terrainrigidbody import TerrainRigidBody
-from selector.terrainselector import TerrainSelector
 from terraincolision import TerrainCollision
 from direct.showbase.ShowBase import ShowBase
-from entities.parts.mobility import BasicTracks
-from entities.modules.turret import CannonTurret
 from maps.terrainprovider import TerrainProvider
 
 
@@ -33,14 +30,12 @@ class MyApp( ShowBase ):
 		self.terrain.getRoot().reparentTo( self.render )
 		self.terrain.setFocalPoint( self.camera )
 		self.disableMouse()
-		self.terrainCamera = TerrainCamera( self.camera, self.terrainInfo.terrainCenter, self.terrainInfo.terrainSize )
-		self.cameraButtons = CameraButtons( self.terrainCamera )
-		self.lights = Lights( self.render )
-
 		# Set up Bullet physics world
 		self.physics_world = BulletWorld()
 		self.physics_world.setGravity( Vec3( 0, 0, -9.81 ) )
-		#self.enable_debug_visualization()
+		self.terrainCamera = TerrainCamera( self.camera, self.terrainInfo.terrainCenter, self.terrainInfo.terrainSize )
+		self.cameraButtons = CameraButtons( self.terrainCamera, debugNode = self.get_debug_visualization() )
+		self.lights = Lights( self.render )
 		self.createCollisionLayer( terrain = self.terrain )
 		self.terrainPhysicsLayer = self.createPhysicsLayer( blockSize = 128 )
 
@@ -52,16 +47,12 @@ class MyApp( ShowBase ):
 				terrainCamera = self.terrainCamera,
 				physicsWorld = self.physics_world,
 				render = self.render )
+		self.__entityLoader = EntityLoader( render = self.render, physicsWorld = self.physics_world, loader = self.loader )
+		self.entityButtons = EntityButtons( selector = self.__selector, loader = self.__entityLoader )
 
-		engine = BasicEngine()
-		turret = CannonTurret()
-		mobility = BasicTracks()
-		self.tank = Tank( engine = engine, turret = turret, mobility = mobility )
-		self.tank.buildModels( self.loader )
 
 		self.task_duration = 0.2
 		self.accept( 'mouse1', self.on_map_click )
-		self.accept( 'mouse3', self.on_map_loader_click )
 		#self.taskMgr.add( self.updateMouseTask, 'updateMouseTask' )
 		self.taskMgr.add( self.terrainCamera.updateCameraTask, "UpdateCameraTask" )
 		#self.taskMgr.add( self.check_collisions, "check_collisions" )
@@ -80,26 +71,23 @@ class MyApp( ShowBase ):
 	def on_map_click( self ):
 		self.__selector.on_map_click()
 
-	def on_map_loader_click( self ):
-		self.__selector.on_map_loader_click( self.tank )
-
 	def updateMouseTask( self, task ):
 		self.update_mouse_hover()
 		task.delayTime = self.task_duration
-		# Return task.again to schedule the task with the new delay
 		return Task.again
 
 	def update_mouse_hover( self ):
 		self.__selector.on_map_hover()
 
-	def enable_debug_visualization( self ):
+	def get_debug_visualization( self ):
 		debug_node = BulletDebugNode( 'Debug' )
 		debug_node.showWireframe( True )
 		debug_node.showBoundingBoxes( True )
 		debug_np = self.render.attachNewNode( debug_node )
 		debug_np.setColor( Color.RED.value )
-		debug_np.show()
+		#debug_np.show()
 		self.physics_world.setDebugNode( debug_np.node() )
+		return debug_np
 
 	def check_collisions( self, task ):
 		"""Check for collisions and print results."""
