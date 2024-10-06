377b0f9b41ca24175f35e2fcecf97809ffc1a287
Gadi Klein <gadik@liveu.tv>
2024-10-06 17:14:38 +0300

    rolls

--- entities/parts/part.py
+++ entities/parts/part.py
@@ -1,22 +1,40 @@
-from entities.entity import PartFactory
+import os.path
+
+from panda3d.core import LColor
+
+from enums.colors import Color
 
 
 class Part:
-    def __init__( self, partData, partId, external: bool = False, **kwargs ) :
+    def __init__( self, partData = None, partId: str = None, external: bool = False, isRendered: bool = True, device = None, **kwargs ) :
+        self._rigidBodyMask = 0
+        self.__device = device
         self._external = external
-        self.__partFactory = None
-        part_data = partData.get( partId )
-        self._readPartData( part_data )
-        if self.hasParts :
-            self.__partFactory = PartFactory( self )
+        self.__partId = partId
+        self.__isRendered = isRendered
+        self._color = Color.WHITE.value
+        if partId and partData:
+            part_data = partData.get( partId )
+            self._readPartData( part_data )
 
     @property
-    def hasParts( self ) -> bool :
-        return False
+    def objectPath( self ) -> str:
+        return ''
 
     @property
-    def partFactory( self ):
-        return self.__partFactory
+    def isRendered( self ) -> bool:
+        return self.__isRendered
+
+    def rigidBodyMask( self ) -> int:
+        return self._rigidBodyMask
+
+    @property
+    def color( self ):
+        return self._color
+
+    @property
+    def partId( self ) -> str:
+        return self.__partId
 
     def _readPartData( self, part_data ) :
         self.__metal = part_data[ "metal" ]
@@ -24,4 +42,3 @@ class Part:
         if self._external:
             self.__protection = part_data[ "protection" ]
             self.__damage = part_data[ "damage" ]
-            self._renderId = None
