5e1a6458d1b9d616a55e8de0a388723eb118e121
Gadi Klein <gadik@liveu.tv>
2025-01-12 15:21:27 +0200

    attacker

---
+++ entities/full/attacker.py
@@ -0,0 +1,11 @@
+
+
+class Attacker:
+    def __init__( self ):
+        self.attackTarget = None
+
+    def attack( self, target ):
+        pass
+
+    def scheduleAttackingTasks( self ):
+        pass
