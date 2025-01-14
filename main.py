78d5765285f187d3eaf7b2bbd0b304bd4b516491
Gadi Klein <gadik@liveu.tv>
2025-01-14 17:13:24 +0200

    target

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ class MyApp( ShowBase ):
 		self.mapName = "heightmap1"
 		self.showTexture = True
 		if True:
-			self.mapName = "heightmap_big"
+			self.mapName = "heightmap_flat_big"
 			self.showTexture = True
 		ShowBase.__init__( self )
 		self.__terrainPhysicsLayer = None
