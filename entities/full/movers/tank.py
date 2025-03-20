3fe4e5df99b3d063927d4551773cf4a9af0aa253
Gadi Klein <gadik@liveu.tv>
2025-03-20 17:11:19 +0200

    refactor

--- entities/full/movers/tank.py
+++ entities/full/movers/tank.py
@@ -1,38 +1,17 @@
-from panda3d.bullet import BulletHingeConstraint
-from panda3d.core import Vec3
-
 from entities.entity import entitypart
+from entities.modules.turret import Turret
 from entities.full.attacker import Attacker
 from entities.full.movers.mover import Mover
 from entities.modules.chassis import Chassis
-from entities.modules.turret import Turret
+from entities.full.entitywithturret import EntityWithTurret
 
 
-class Tank( Mover, Attacker ):
+class Tank( Mover, Attacker, EntityWithTurret ):
     def __init__( self, engine, chassis: Chassis, turret: Turret ):
         super().__init__( chassis = chassis, engine = engine )
-        self.__turret = turret
+        EntityWithTurret.__init__( self, chassis, turret )
 
-    @entitypart
-    def turretBase( self ):
-        return self.__turret.turretBase
 
     @entitypart
     def cannon( self ):
-        return self.__turret.turretCannon
-
-    def _connectModules( self, world ):
-        pivot_in_hull = Vec3( 0, 0, 1 )
-        axis_in_hull = Vec3( 0, 0, 1 )
-        pivot_in_turret = Vec3( 0, 0, 0 )
-        axis_in_turret = Vec3( 0, 0, 1 )
-
-        hinge = BulletHingeConstraint(
-            self._chassis.hull().rigidBody,
-            self.turretBase().rigidBody,
-            pivot_in_hull, axis_in_hull,
-            pivot_in_turret, axis_in_turret,
-        )
-        hinge.setLimit( 0, 0 )
-        hinge.setBreakingThreshold( float( 'inf' ) )
-        world.attachConstraint( hinge )
+        return self._turret.turretCannon
